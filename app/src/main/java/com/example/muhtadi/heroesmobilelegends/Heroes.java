cdc7cf69baea2db764459ca201b811c5919ab95f
Muhtadi Irfan <muhtadi.irfan@gmail.com>
2018-04-23 14:55:50 +0700

    adding getter and setter Heroes Object

---
+++ app/src/main/java/com/example/muhtadi/heroesmobilelegends/Heroes.java
@@ -0,0 +1,49 @@
+package com.example.muhtadi.heroesmobilelegends;
+
+/**
+ * Created by Muhtadi on 23/04/2018.
+ */
+
+public class Heroes {
+    private String name, category, photo, image, story;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+}
