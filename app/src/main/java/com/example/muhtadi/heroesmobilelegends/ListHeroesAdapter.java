4dedbeacc7728c41a638d02d4c0d4bfe988823e4
Muhtadi Irfan <muhtadi.irfan@gmail.com>
2018-04-23 15:41:47 +0700

    create adapter class and adding recyclerView in activity_main.xml

---
+++ app/src/main/java/com/example/muhtadi/heroesmobilelegends/ListHeroesAdapter.java
@@ -0,0 +1,71 @@
+package com.example.muhtadi.heroesmobilelegends;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Muhtadi on 23/04/2018.
+ */
+
+public class ListHeroesAdapter extends RecyclerView.Adapter<ListHeroesAdapter.CategoryViewHolder>{
+    private Context context;
+
+    public ArrayList<Heroes> getListPresident() {
+        return listHeroes;
+    }
+    public void setListPresident(ArrayList<Heroes> listPresident) {
+        this.listHeroes = listPresident;
+    }
+
+    private ArrayList<Heroes> listHeroes;
+
+    public ListHeroesAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public CategoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemRow = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_heroes, parent, false);
+        return new CategoryViewHolder(itemRow);
+    }
+
+    @Override
+    public void onBindViewHolder(CategoryViewHolder holder, int position) {
+
+        holder.tvName.setText(getListPresident().get(position).getName());
+        holder.tvCategory.setText(getListPresident().get(position).getCategory());
+
+        Glide.with(context)
+                .load(getListPresident().get(position).getPhoto())
+                .override(55, 55)
+                .crossFade()
+                .into(holder.imgPhoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return getListPresident().size();
+    }
+
+    class CategoryViewHolder extends RecyclerView.ViewHolder{
+        TextView tvName;
+        TextView tvCategory;
+        ImageView imgPhoto;
+
+        public CategoryViewHolder(View itemView) {
+            super(itemView);
+            tvName = (TextView)itemView.findViewById(R.id.tv_name);
+            tvCategory = (TextView)itemView.findViewById(R.id.tv_category);
+            imgPhoto = (ImageView)itemView.findViewById(R.id.img_photo);
+        }
+    }
+}
